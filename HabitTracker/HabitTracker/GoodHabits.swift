bf2eb99ed17a956c44f20c723b32847ef59bf042
cjay-m <christianjmontalvo@gmail.com>
2023-05-23 18:47:40 -0700

    cjay-m May 23 changes

---
+++ HabitTracker/HabitTracker/GoodHabits.swift
@@ -0,0 +1,8 @@
+//
+//  GoodHabits.swift
+//  HabitTracker
+//
+//  Created by cjay on 5/23/23.
+//
+
+import Foundation
---
+++ HabitTracker/HabitTracker/View/SuggestedHabit.swift
@@ -0,0 +1,8 @@
+//
+//  SuggestedHabit.swift
+//  HabitTracker
+//
+//  Created by cjay on 5/23/23.
+//
+
+import Foundation
